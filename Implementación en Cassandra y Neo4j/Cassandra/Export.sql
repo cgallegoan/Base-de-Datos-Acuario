d4fde6b19434ac5a72af577d6188bf52314375f7
Carlos Gallego Andreu <104354608+cgallegoan@users.noreply.github.com>
2022-04-26 18:13:24 +0200

    Update Export.sql

--- Implementación en Cassandra y Neo4j/Cassandra/Export.sql
+++ Implementación en Cassandra y Neo4j/Cassandra/Export.sql
@@ -43,6 +43,48 @@ SELECT 1 as agrupa, nombre_cientifico, cod_especie, lugar_origen, clase, aliment
 FROM especie
 ORDER BY nombre_cientifico ASC;
 
+-- PECERA_POR_ESPECIE
+SELECT cod_especie, nombre_cientifico, nombre, situacion, superficie
+FROM especie, pecera, espacio
+WHERE especie.cod_pec = pecera.cod_pec and espacio.cod_espacio = pecera.cod_pec
+ORDER BY nombre_cientifico ASC;
+
+-- EJEMPLAR_POR_ESPECIE
+SELECT ej.cod_especie, ej.nombre, cod_ejemplar, importado, fecha_inc, peso
+FROM ejemplar ej, especie e
+WHERE ej.cod_especie = e.cod_especie
+ORDER BY ej.nombre ASC;
+
+-- CUIDADOR_POR_ESPECIE
+SELECT e.cod_especie, emp.nombre, emp.anyo_exp, emp.sueldo FROM cuidador c, especie e, cuidado cu, empleado emp
+WHERE c.Nº_EMPLEADO = cu.nº_cuid and e.cod_especie = cu.cod_especie 
+and emp.nº_empleado = c.nº_empleado
+ORDER BY emp.nombre ASC, emp.anyo_exp ASC;
+
+-- ENCARGADO_POR_NOMBRE
+SELECT 1 as agrupa, e.nombre, e.nº_empleado, e.anyo_exp, e.NIF, e.sueldo
+FROM encargado en, empleado e
+WHERE en.nº_empleado = e.nº_empleado
+ORDER BY e.nombre DESC;
+
+-- ESP_FUNC_POR_ENCARGADO
+SELECT emp.nº_empleado, emp.nombre as nombre_encargado, e.nombre as nombre_espacio, e.superficie, e.situacion, LISTAGG(f.funcion, ', ') 
+WITHIN GROUP(ORDER BY f.funcion) as funciones
+FROM empleado emp, espacio e, funcion f, encargo en
+WHERE emp.nº_empleado = en.nº_empleado and en.cod_espacio = e.cod_espacio and e.cod_espacio = f.cod_espacio
+GROUP BY emp.nº_empleado, e.nombre, e.superficie, e.situacion, emp.nombre
+ORDER BY e.superficie DESC, e.nombre ASC;
+
+-- EMPLEADO_POR_NOMBRE
+SELECT 1 as agrupa, e.nombre, e.nº_empleado, e.nº_emp_JEFE, e.anyo_exp, e.NIF, e.sueldo
+FROM empleado e
+ORDER BY e.nombre ASC;
+
+-- SUBS_POR_EMPLEADO
+SELECT nº_emp_jefe,sueldo,nº_empleado
+FROM empleado
+
+
 
 
 
